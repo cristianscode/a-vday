55acbdfd64b6450dcf86a155876decef1286926c
cristianscode <cristianscode@gmail.com>
2022-02-13 16:29:18 -0800

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import * as $ from 'jquery'
+import { TweenLite, TweenMax, Linear, Sine } from 'gsap';
+import gsap from 'gsap';
+import { FlowerComponent } from './flower/flower.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild("flower") flower: FlowerComponent;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      $("#back").fadeOut();
+      $("#heart").fadeOut();
+      gsap.registerPlugin(TweenLite, TweenMax, Linear, Sine);
+
+      TweenLite.set("#content-section", { perspective: 600 })
+      // TweenLite.set("#img", { xPercent: "-50%", yPercent: "-50%" })
+      var total = 10;
+      var warp = document.getElementById("content-section");
+      var w = window.innerWidth;
+      for (let i = 0; i < total; i++) {
+        var Div = document.createElement('div');
+        var size = "dot";
+        TweenLite.set(Div, { attr: { class: size }, x: this.R(0, w), y: this.R(-200, -150), z: this.R(-200, 200) });
+        warp?.appendChild(Div);
+        this.animm(Div);
+      }
+    }, 2000)
+  }
+
+  animm(elm: any) {
+    var h = window.innerHeight;
+    TweenMax.to(elm, this.R(6, 15), { y: h + 100, ease: Linear.easeNone, repeat: -1, delay: -15 });
+    TweenMax.to(elm, this.R(4, 8), { x: '+=100', rotationZ: this.R(0, 180), repeat: -1, yoyo: true, ease: Sine.easeInOut });
+    TweenMax.to(elm, this.R(2, 8), { rotationX: this.R(0, 360), rotationY: this.R(0, 360), repeat: -1, yoyo: true, ease: Sine.easeInOut, delay: -5 });
+  };
+
+  R(min: any, max: any) { return min + Math.random() * (max - min) };
+
+  next(current: string, next: string) {
+    $("#" + current).fadeOut('slow');
+    setTimeout(() => { $("#" + next).fadeIn('slow') }, 1000);
+    if (next == 'step9') {
+      $(".dot").fadeOut('slow');
+      $("#back").fadeIn();
+      $("#heart").fadeIn();
+      setTimeout(() => {
+        this.flower.init();
+        $("#back").fadeOut();
+        $("#heart").fadeOut();
+      }, 2000);
+    }
+
+
+  }
   title = 'a-vday';
 }
---
+++ src/app/flower/flower.component.ts
@@ -0,0 +1,179 @@
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import * as THREE from "three";
+import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-flower',
+  templateUrl: './flower.component.html',
+  styleUrls: ['./flower.component.css']
+})
+export class FlowerComponent implements AfterViewInit {
+  private renderer: THREE.WebGLRenderer;
+  private camera: THREE.PerspectiveCamera;
+  public scene: THREE.Scene;
+
+  public fieldOfView: number = .3;
+  public nearClippingPane: number = 1;
+  public farClippingPane: number = 1100;
+
+  public controls: OrbitControls;
+
+  @ViewChild('canvas')
+  private canvasRef: ElementRef;
+
+  constructor() {
+    this.render = this.render.bind(this);
+    this.onModelLoadingCompleted = this.onModelLoadingCompleted.bind(this);
+  }
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  private createScene() {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color('#ffe6f2')
+    var loader = new GLTFLoader();
+    loader.load('assets/scene.gltf', this.onModelLoadingCompleted);
+  }
+
+  private onModelLoadingCompleted(collada: { scene: any; }) {
+    const box = new THREE.Box3().setFromObject(collada.scene);
+    const center = box.getCenter(new THREE.Vector3());
+    collada.scene.position.x += (collada.scene.position.x - center.x);
+    collada.scene.position.y += (collada.scene.position.y - center.y) - 0.10;
+    collada.scene.position.z += (collada.scene.position.z - center.z);
+
+    var modelScene = collada.scene;
+    this.scene.add(modelScene);
+    this.render();
+  }
+
+  private createLight() {
+    var light = new THREE.PointLight(0xffffff, 3, 1000);
+    light.position.set(0, 0, 100);
+    this.scene.add(light);
+
+    var light = new THREE.PointLight(0xffffff, 2, 1000);
+    light.position.set(0, 0, -100);
+    this.scene.add(light);
+  }
+
+  private createCamera() {
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPane,
+      this.farClippingPane
+    );
+
+    // Set position and look at
+    this.camera.position.x = -50;
+    this.camera.position.y = 50;
+    this.camera.position.z = 100;
+
+  }
+
+  private getAspectRatio(): number {
+    let height = this.canvas.clientHeight;
+    if (height === 0) {
+      return 0;
+    }
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+  private startRendering() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true
+    });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+
+    this.renderer.setClearColor(0xffffff, 1);
+    this.renderer.autoClear = true;
+
+    let component: FlowerComponent = this;
+
+    (function render() {
+      requestAnimationFrame(render);
+      component.render();
+    }());
+  }
+
+  public render() {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  public addControls() {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.rotateSpeed = 0.4;
+    this.controls.zoomSpeed = 0.4;
+    this.controls.addEventListener('change', this.render);
+
+  }
+
+  /* EVENTS */
+
+  public onMouseDown(event: MouseEvent) {
+    event.preventDefault();
+
+    // Example of mesh selection/pick:
+    var raycaster = new THREE.Raycaster();
+    var mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / this.renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = - (event.clientY / this.renderer.domElement.clientHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, this.camera);
+
+    var obj: THREE.Object3D[] = [];
+    this.findAllObjects(obj, this.scene);
+    var intersects = raycaster.intersectObjects(obj);
+    intersects.forEach((i) => { });
+  }
+
+  private findAllObjects(pred: THREE.Object3D[], parent: THREE.Object3D) {
+    // NOTE: Better to keep separate array of selected objects
+    if (parent.children.length > 0) {
+      parent.children.forEach((i) => {
+        pred.push(i);
+        this.findAllObjects(pred, i);
+      });
+    }
+  }
+
+  public onMouseUp(event: MouseEvent) {
+    console.log("onMouseUp");
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event: Event) {
+    this.canvas.style.width = "100%";
+    this.canvas.style.height = "100%";
+    this.camera.aspect = this.getAspectRatio();
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+    this.render();
+  }
+
+  @HostListener('document:keypress', ['$event'])
+  public onKeyPress(event: KeyboardEvent) {
+    console.log("onKeyPress: " + event.key);
+  }
+
+  /* LIFECYCLE */
+  public ngAfterViewInit(): void {
+  }
+
+  public init() {
+    this.createScene();
+    this.createLight();
+    this.createCamera();
+    this.startRendering();
+    this.addControls();
+  }
+}
